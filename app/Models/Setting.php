2467b173c35b60b1ddae910e2e0213b77d732ceb
Mohamadreza Rezaei <clash.trial@gmail.com>
2023-09-07 20:25:52 +0330

    add setting table for save current user token

---
+++ app/Models/Setting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'current_user_id',
+        'brear_token',
+    ];
+}
