6b08deb87908587b316a099365e4da41406141fe
moham7dreza <clash.trial@gmail.com>
2023-09-20 12:06:48 +0330

    add swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\PathItem(path="/api")
+ * @OA\Info(
+ *   title="Marketplace Api Routes List",
+ *   version="1.0.0"
+ * )
+ * This class should be parent class for other API controllers
+ * Class AppBaseController
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
